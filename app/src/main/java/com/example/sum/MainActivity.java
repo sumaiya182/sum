5a1e3db4108b2c9fba7408cb0659e503a685ccf5
Sumaiya182 <asumiya158@gmail.com>
2020-07-14 18:52:41 +0600

    hello

---
+++ app/src/main/java/com/example/sum/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.sum;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText input1;
+    private EditText input2;
+    private Button addBtn;
+    private TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        input1 = (EditText)findViewById(R.id.input1);
+        input2 = (EditText)findViewById(R.id.input2);
+        addBtn = (Button)findViewById(R.id.button);
+        result = (TextView) findViewById(R.id.result);
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt(input1.getText().toString());
+                int b = Integer.parseInt(input2.getText().toString());
+                int c = a + b;
+                result.setText("Result: " + String.valueOf(c));
+            }
+        });
+    }
+}
