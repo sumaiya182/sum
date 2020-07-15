6114b9acaa3d1fe8a2babe0ec0348d02cf378bca
Sumaiya182 <asumiya158@gmail.com>
2020-07-15 19:53:11 +0600

    first page done

--- app/src/main/java/com/example/sum/FirstPageActivity.java
+++ app/src/main/java/com/example/sum/FirstPageActivity.java
@@ -2,6 +2,7 @@ package com.example.sum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -24,6 +25,7 @@ public class FirstPageActivity extends AppCompatActivity {
 
     public void openMainActivity()
     {
-
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
