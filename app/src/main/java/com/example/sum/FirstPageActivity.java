aa0ed2370068dd2f240465e91b734ebd157d4f36
Sumaiya182 <asumiya158@gmail.com>
2020-07-15 19:51:05 +0600

    code added to first page

--- app/src/main/java/com/example/sum/FirstPageActivity.java
+++ app/src/main/java/com/example/sum/FirstPageActivity.java
@@ -3,12 +3,27 @@ package com.example.sum;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FirstPageActivity extends AppCompatActivity {
-
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+
+        btn = (Button)findViewById(R.id.btnGo);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity()
+    {
+
     }
 }
